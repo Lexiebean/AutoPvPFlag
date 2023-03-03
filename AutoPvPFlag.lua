18332c3ac1ba52e510588b02542e4bd2231f2bc2
Lexie <6937574+Lexiebean@users.noreply.github.com>
2023-03-04 06:53:08 +1100

    Update AutoPvPFlag.lua

--- AutoPvPFlag.lua
+++ AutoPvPFlag.lua
@@ -1,5 +1,5 @@
 local frame=CreateFrame("Frame");
 frame:RegisterEvent("VARIABLES_LOADED");
 frame:SetScript("OnEvent",function(self,event,...)
-	DEFAULT_CHAT_FRAME.editBox:SetText("/pvp") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
+	TogglePVP()
 end);
